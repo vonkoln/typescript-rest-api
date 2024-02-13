79f8ae44a8937abc037fdf4f08425e4cf8e42aa1
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2024-02-13 19:59:40 +0000

    ..

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,17 @@ import compression from 'compression';
 import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const servidor = http.createServer(app);
+
+servidor.listen(8080, () => {
+    console.log("Servidor rodando no endereço http://localhost:8080/")
+})
